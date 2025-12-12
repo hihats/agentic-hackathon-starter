7470dcb13bc9a61044c4f88b3867f874e694534d
kurano <nokori114117@gmail.com>
2025-12-12 05:27:55 +0000

    Nuxt と Go の環境追加

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>Welcome to Nuxt</h1>
+    <p>This is a Nuxt 3 application running in Docker.</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+}
+
+h1 {
+  color: #00dc82;
+}
+</style>
